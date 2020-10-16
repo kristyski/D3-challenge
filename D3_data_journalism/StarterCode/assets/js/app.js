4c8421691ebb867d5363009f03f68625a96378d9
Autzen <kristyskisfast@gmail.com>
2020-10-16 14:06:00 -0700

    updates

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,33 +1,52 @@
 // @TODO: YOUR CODE HERE! from 16/03/09 1st hair
-var svgWidth = 960;
-var svgHeight = 500;
 
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 20 //was 100
-};
+//12/extra responsive
+d3.select(window).on("resize", handleResize);
 
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+// When the browser loads, loadChart() is called
+loadChart();
 
-// Create an SVG wrapper, append an SVG group that will hold the chart, and shift the chart by left and top margins.
-var svg = d3
-  .select("#svg-area") //was .chart, .scatter didn't work
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+function handleResize() {
+  var svgArea = d3.select("svg");
+
+  // If there is already an svg container on the page, remove it and reload the chart
+  if (!svgArea.empty()) {
+    svgArea.remove();
+    loadChart();
+  }
+}
+
+function loadChart() {
+
+  var svgWidth = window.innerWidth -5; //was = 960;
+  var svgHeight = window.innerHeight -5; //was = 500;
+
+  var margin = {
+    top: 20,
+    right: 20,
+    bottom: 60,
+    left: 100 //was 100
+  };
+
+  var width = svgWidth - margin.left - margin.right;
+  var height = svgHeight - margin.top - margin.bottom;
+
+  // Create an SVG wrapper, append an SVG group that will hold the chart, and shift the chart by left and top margins.
+  var svg = d3
+    .select("#svg-area") //was .chart, .scatter didn't work
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
   // Append an SVG group
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
   // .classed("bar", true) add somewhere ??
 
 
-// Import Data
-d3.csv("./assets/data/data.csv").then(censusData => {
+  // Import Data
+  d3.csv("./assets/data/data.csv").then(censusData => {
 
     // Step 1: Parse Data/Cast as numbers
     // ==============================
@@ -39,7 +58,7 @@ d3.csv("./assets/data/data.csv").then(censusData => {
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(censusData, d => d.smokes)])
+      .domain([8, d3.max(censusData, d => d.smokes)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
@@ -63,36 +82,36 @@ d3.csv("./assets/data/data.csv").then(censusData => {
     // Step 5: Create Circles
     // ==============================
     var circlesGroup = chartGroup.selectAll("circle")
-    .data(censusData)
-    .join("circle")
-    .attr("cx", d => xLinearScale(d.smokes))
-    .attr("cy", d => yLinearScale(d.age))
-    .attr("r", "15")
-    .attr("fill", "pink")
-    .attr("fill-opacity", 0.5)
-    .attr("stroke", "black")
-    .attr("stroke-width", 1);
+      .data(censusData)
+      .join("circle")
+      .attr("cx", d => xLinearScale(d.smokes))
+      .attr("cy", d => yLinearScale(d.age))
+      .attr("r", "15")
+      .attr("fill", "yellow")
+      .attr("fill-opacity", 0.75)
+      .attr("stroke", "grey")
+      .attr("stroke-width", 1);
 
     // Step 6: Initialize tool tip
     // ==============================
-    // var toolTip = d3.tip()
-    //   .attr("class", "tooltip")
-    //   .offset([80, -60])
-    //   .html(d => `${d.state}<br>Smokes: ${d.smokes}<br>Age: ${d.age}`);
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(d => `${d.state}<br>Smokes: ${d.smokes}<br>Age: ${d.age}`);
 
     // Step 7: Create tooltip in the chart
     // ==============================
-    // chartGroup.call(toolTip);
+    chartGroup.call(toolTip);
 
     // Step 8: Create event listeners to display and hide the tooltip
     // ==============================
-    // circlesGroup.on("click", function(data) {
-    //   toolTip.show(data, this);
-    // })
-    //   // onmouseout event
-    //   .on("mouseout", function(data) {
-    //     toolTip.hide(data);
-    //   });
+    circlesGroup.on("click", function (data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function (data) {
+        toolTip.hide(data);
+      });
 
     // Create axes labels
     chartGroup.append("text")
@@ -109,3 +128,4 @@ d3.csv("./assets/data/data.csv").then(censusData => {
       .text("Age (years)");
 
   }).catch(error => console.log(error));
+}
